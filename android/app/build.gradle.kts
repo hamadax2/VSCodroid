879a769ad91c425a5efeee6970d23973c3a1408c
hamadax2 <hamadax1999@yahoo.com>
2026-09-15 16:51:10 +0200

    Fix JUnit Jupiter API dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.junit.jupiter.api)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
